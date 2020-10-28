7e8b7b4a7cef9b288b74b54be3c6382da932868f
andiwoedwin <andiwoedwin@gmail.com>
2020-10-28 12:15:08 +0300

    add image to class profile

--- insta/models.py
+++ insta/models.py
@@ -22,22 +22,22 @@ class Post(models.Model):
 
 
 
-class Review(models.Model):
-    ratings = (1, 1),(2, 2),(3, 3),(4, 4),(5, 5),(6, 6),(7, 7),(8, 8),(9, 9),(10, 10)
+# class Review(models.Model):
+#     ratings = (1, 1),(2, 2),(3, 3),(4, 4),(5, 5),(6, 6),(7, 7),(8, 8),(9, 9),(10, 10)
     
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='reviews')
-    design = models.IntegerField(choices=ratings, default=0)
-    usability = models.IntegerField(choices=ratings, default=0)
-    creativity = models.IntegerField(choices=ratings, default=0)
-    content =  models.IntegerField(choices=ratings, default=0)
-    overall_score = models.IntegerField(blank=True, default=0)
+#     user = models.ForeignKey(User,on_delete=models.CASCADE)
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='reviews')
+#     design = models.IntegerField(choices=ratings, default=0)
+#     usability = models.IntegerField(choices=ratings, default=0)
+#     creativity = models.IntegerField(choices=ratings, default=0)
+#     content =  models.IntegerField(choices=ratings, default=0)
+#     overall_score = models.IntegerField(blank=True, default=0)
 
-    def save_rate(self):
-            self.save()
+#     def save_rate(self):
+#             self.save()
 
-    def delete_rate(self):
-        self.delete()
+#     def delete_rate(self):
+#         self.delete()
 
-    def get_absolute_url(self):
-        return reverse('index')
+#     def get_absolute_url(self):
+#         return reverse('index')
