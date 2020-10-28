5c81384d4aadf0d38c611c799fdc5e0908a99afe
andiwoedwin <andiwoedwin@gmail.com>
2020-10-28 12:13:55 +0300

    register profile

--- insta/models.py
+++ insta/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
-
+    url = models.CharField(max_length=50, blank=True)
 
    
 
@@ -19,3 +19,25 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+
+class Review(models.Model):
+    ratings = (1, 1),(2, 2),(3, 3),(4, 4),(5, 5),(6, 6),(7, 7),(8, 8),(9, 9),(10, 10)
+    
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='reviews')
+    design = models.IntegerField(choices=ratings, default=0)
+    usability = models.IntegerField(choices=ratings, default=0)
+    creativity = models.IntegerField(choices=ratings, default=0)
+    content =  models.IntegerField(choices=ratings, default=0)
+    overall_score = models.IntegerField(blank=True, default=0)
+
+    def save_rate(self):
+            self.save()
+
+    def delete_rate(self):
+        self.delete()
+
+    def get_absolute_url(self):
+        return reverse('index')
